12d25135f84a8baecabbbbc09d64371c35946c0d
khemroat <khemroat@gmail.com>
2017-07-25 21:29:05 -0700

    added primitive type

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# core
+# Core
 concept
+
+## Content
+### Basic
+- <a href="https://github.com/roat167/core/blob/master/src/io/kapp/core/java/intro/Main.java">Hello World</a>
+- <a href="https://github.com/roat167/core/blob/master/src/io/kapp/core/java/basic/PrimitiveType.java">Primitive Types</a>
---
+++ src/io/kapp/core/java/basic/PrimitiveType.java
@@ -0,0 +1,29 @@
+package io.kapp.core.java.basic;
+
+public class PrimitiveType {
+
+/**
+ * Primitives always have a value, they can never be null
+ * If not defined they will take default value 0, and false for boolean
+ * Suffix: Good practice is to use uppercase 
+ *	L for long
+ * 	F for float 
+ * 	D for double (optional)
+ *
+ * Primitives and size in BITS
+ * 	boolean		1
+ * 	byte		8	(1 byte)
+ * 	short		16	(2 byte)
+ * 	int 		32	(4 byte)
+ * 	long 		64	(8 byte)
+ * 	float		32	(4 byte)
+ * 	double 		64	(8 byte)
+ * 	char		16 (unsigned) default value '\u0000'
+ */
+
+	public static void main(String[] args) {
+		//int val = Integer.MAX_VALUE;
+		//long biggerVal = val + 1;
+		
+	}
+}
